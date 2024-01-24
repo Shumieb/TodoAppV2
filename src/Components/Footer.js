c4707d7bb3b25d7ca641b0e97a99be1b307612ca
Shumieb <shumiebc@gmail.com>
2024-01-24 03:05:21 +0000

    Added Styles to Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <div className='flex flex-row p-2 bg-darkTeal text-lightSlate mt-4 justify-center align-center text-lg'>
+            <p className='p-1 mx-2'>&copy; 2024</p>
+            <p className='p-1 mx-2'>Created By: Shumie</p>
+        </div>
     )
 }
 
